acbc1137ea95749e29db7374869b26227d4b046e
nikhil24min <61593368+nikhil24min@users.noreply.github.com>
2022-08-06 21:38:01 +0530

    Update oneWeekPreparationKit.js

--- oneWeekPreparationKit.js
+++ oneWeekPreparationKit.js
@@ -27,3 +27,20 @@ function plusMinus(arr) {
     console.log(n.toFixed(6))
     console.log(z.toFixed(6))
 }
+
+
+//// Problem 2.. Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+//// Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let max = Math.max(...arr);
+    let min = Math.min(...arr)
+    
+    arr.splice(arr.indexOf(max),1);
+    arr.splice(arr.indexOf(min),1);
+    
+    let sum = arr.reduce((partialSum, a) => partialSum + a, 0);
+    
+    console.log(String(sum+min)+" "+String(sum+max))
+}
